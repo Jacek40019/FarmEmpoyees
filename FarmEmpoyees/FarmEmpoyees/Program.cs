795aa4a1368aa8cc3ab760c7b7d408df2e12f985
Jacek Zimoląg <jacek.zimolag1@gmail.com>
2024-01-16 22:06:46 +0100

    Consol app - FarmEmployees

---
+++ FarmEmpoyees/FarmEmpoyees/Program.cs
@@ -0,0 +1,7 @@
+﻿// Application for calculating payments of fruit plantation employees
+
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("                              FarmEmployees - payroll");
+Console.WriteLine("--------------------------------------------------------------------------------------------");
+Console.ResetColor();
